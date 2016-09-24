6f95fdd9d8ecae3438d69d8760dd4eed9687137d
y_okada <yasokada@gmail.com>
2016-09-24 15:22:10 +0900

    put 20 particles randomly

---
+++ Assets/makeDroxtal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * v0.1 Sep. 24, 2016
+ *   - put 20 particles randomly
+ */
+
+public class makeDroxtal : MonoBehaviour {
+
+	public GameObject my_obj;
+
+	void Start () {
+		arrayParticles ();	
+	}
+
+	void arrayParticles() {
+		for (int idx = 0; idx < 20; idx++) {
+			GameObject instance = (GameObject)Instantiate (my_obj);	
+			instance.transform.position = my_obj.transform.position;
+
+			Vector3 pos = new Vector3 ();
+			pos.x = Random.Range (0, 2f);
+			pos.y = Random.Range (0, 2f);
+			pos.z = Random.Range (0, 2f);
+			instance.transform.position = pos;
+		}
+	}
+	
+	void Update () {
+
+	}
+}
