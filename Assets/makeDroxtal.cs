5587cb4d7b353eb0b411c649a5c6f438e0d7e5d4
y_okada <yasokada@gmail.com>
2016-09-24 15:37:54 +0900

    add calcVertices() using random position

--- Assets/makeDroxtal.cs
+++ Assets/makeDroxtal.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 /*
  * v0.1 Sep. 24, 2016
+ *   - add calcVertices() using random position
+ *   - add [kNumVerticles]
  *   - add CameraRotation script on [Main Camera]
  *   - put 20 particles randomly
  */
@@ -11,20 +13,27 @@ public class makeDroxtal : MonoBehaviour {
 
 	public GameObject my_obj;
 
+	public const int kNumVerticles = 20;
+
+	Vector3[] vertices = new Vector3[kNumVerticles];
+
+	void calcVertices() {
+		for (int idx = 0; idx < kNumVerticles; idx++) {
+			vertices [idx].x = Random.Range (0, 2f) - 1f;
+			vertices [idx].y = Random.Range (0, 2f) - 1f;
+			vertices [idx].z = Random.Range (0, 2f) - 1f;
+		}
+	}
+
 	void Start () {
+		calcVertices ();
 		arrayParticles ();	
 	}
 
 	void arrayParticles() {
-		for (int idx = 0; idx < 20; idx++) {
+		for (int idx = 0; idx < kNumVerticles; idx++) {
 			GameObject instance = (GameObject)Instantiate (my_obj);	
-			instance.transform.position = my_obj.transform.position;
-
-			Vector3 pos = new Vector3 ();
-			pos.x = Random.Range (0, 2f) - 1f;
-			pos.y = Random.Range (0, 2f) - 1f;
-			pos.z = Random.Range (0, 2f) - 1f;
-			instance.transform.position = pos;
+			instance.transform.position = vertices [idx];
 		}
 	}
 	
